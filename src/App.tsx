a78bc941d95ed2f58bd7395bc9ae73268976bea7
neverswood <nevers_wood@hotmail.fr>
2023-06-21 21:50:59 +0200

    Add the header component and the home component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { Home } from './page/Home';
+import Header from './components/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
