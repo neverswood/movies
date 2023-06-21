da2f08d225d46041764b3712434363b851e3f10e
neverswood <nevers_wood@hotmail.fr>
2023-06-21 21:54:12 +0200

    Display of film categories in a select and filtering of films by category

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,26 +1,54 @@
-import { MovieCard } from '../components/MovieCard';
 import { MovieCardList } from '../components/MovieCardList';
-import { movies$ } from '../data/DataMovies';
 import { useEffect, useState } from 'react';
 import '../styles/Home.scss';
 import { getMovie } from '../services/Movies';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { updateMovie } from '../features/MovieSlice';
-import FilterCategory from '../components/FilterCategory';
 import { Movie } from '../services/models/MovieDataModel';
-import { GroupBase } from 'react-select';
-import { State } from '../Store';
+import SelectOption from '../components/SelectOption';
+
+/** Home function to display the list of movies and filter them by categories */
 
 export function Home() {
   const [data, setData] = useState<Movie[]>([]);
+  const [option, setOption] = useState('');
+  const dispatch = useDispatch();
+  dispatch(updateMovie(data));
 
   useEffect(() => {
     getMovie().then((response) => setData(response));
   }, []);
 
+  /* Category movie recovery */
+  const options = data.map((movie) => {
+    return movie.category;
+  });
+
+  /* Removal of duplicate movie categories */
+  const uniqueOptions = Array.from(new Set(options));
+
+  /* Creating an array of movie category objects with a label and a value */
+  const uniqueOptionsObject = uniqueOptions.map((option) => {
+    return { label: option, value: option };
+  });
+
+  const filterMovieByCategory = data.filter((movie) =>
+    movie.category.includes(option)
+  );
+
+  dispatch(updateMovie(filterMovieByCategory));
+
   return (
     <div>
-      <MovieCardList data={data} />
+      <SelectOption
+        value={option}
+        placeholder="filter"
+        options={uniqueOptionsObject}
+        onClick={(e) => {
+          setOption(e.value);
+        }}
+      />
+      <MovieCardList data={filterMovieByCategory} />
     </div>
   );
 }
