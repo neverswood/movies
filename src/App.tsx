aea1c852e901ca48b183e5f738de8c8bfbde3836
neverswood <nevers_wood@hotmail.fr>
2023-06-26 00:32:32 +0200

    Data recovery in the home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import { Home } from './page/Home';
 import Header from './components/Header';
+import { getMovie } from './services/Movies';
+import { Movie } from './services/models/MovieDataModel';
 
 function App() {
+  const [data, setData] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    getMovie().then((response) => setData(response));
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Home data={data} />
     </div>
   );
 }
